757eb5b41c8032b892bd9346e0684f1ce931e0c1
saprunovdev <saprunovdev@gmail.com>
2022-12-08 22:32:52 +0900

    learned routing

---
+++ day17/router/src/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from 'react-router-dom'
+
+// Home component
+const Home = (props) => <h1>Welcome Home</h1>
+// About component
+const About = (props) => <h1>About Us</h1>
+// Contact component
+const Contact = (props) => <h1>Contact us</h1>
+// Challenge component
+const Challenges = (props) => (
+  <div>
+    <h1>30 Days Of React Challenge</h1>
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <ul>
+            <li>
+              <NavLink to='/'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink to='/contact'>Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to='/challenges'>Challenges</NavLink>
+            </li>
+          </ul>
+
+          <Routes>
+            <Route path='/about' element={<About/>} />
+            <Route path='/contact' element={<Contact/>} />
+            <Route path='/challenges' element={<Challenges/>} />
+            <Route path='/' element={<Home/>} />
+          </Routes>
+        </div>
+      </Router>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
