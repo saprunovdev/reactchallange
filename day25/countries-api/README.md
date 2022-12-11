93a6a0faaf403686da25c287d8ec77913eaf4b30
saprunovdev <saprunovdev@gmail.com>
2022-12-11 20:01:35 +0900

    started countries API project

---
+++ day25/countries-api/README.md
@@ -0,0 +1,10 @@
+# Getting Started with Create React App
+
+---
+https://www.30daysofreact.com/day-23/countries-data
+
+- Use useState and useEffect to fetch data
+- How to structure the project properly?
+- Fetch countries API
+- Display the countries data and statistics
+- Make a search bar
---
+++ day25/countries-api/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Countries } from './countries';
+
+
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>World Countries Data</h1>
+        <h2>We currently have countires</h2>
+        <Countries/>
+      </header>
+    </>
+  );
+}
+
+export default App;
