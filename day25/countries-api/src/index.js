1ca9d7c5dfd87585bbeee02f04d658fcf91b7dbd
saprunovdev <saprunovdev@gmail.com>
2022-12-13 23:04:51 +0900

    fetched countries data and displayed it

--- day25/countries-api/src/index.js
+++ day25/countries-api/src/index.js
@@ -1,13 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+// import axios from 'axios';
 import './index.css';
-import App from './App';
+
+const Header = ({countriesCount}) =>{
+  return(
+    <div>
+      <h1>World Countries Data</h1>
+      <h2>Currently we have {countriesCount} countries</h2>
+    </div>
+  )
+}
+
+const Country = ({
+  country:{
+    flag,
+    name, 
+    capital,
+    languages,
+    population,
+    currencies,
+  }}) =>{
+
+    return(
+      <div className='country'>
+        <span>{name}: {capital}</span>
+        <div className='country-flag'>
+        <img src={flag} alt={name}/>
+        </div>
+        <p>languages: {languages.map((lang)=> <span>{lang.name} </span>)}</p>
+        <p>Population: {population} </p>
+        <p>Currencies: {currencies
+        ?currencies.map((curr)=> <span>{curr.name} </span>)
+        :<span>No currencies</span>
+        }</p>
+      </div>
+    )
+}
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = (props) => {
+  // setting initial state and method to update state
+  const [data, setData] = useState([])
 
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const url = 'https://restcountries.com/v2/all'
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setData(data)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+    return(
+      <div>
+      <Header countriesCount = {data.length}/>
+
+     <div className='country-wrapper'>
+        {data.map((country)=>(
+          <Country country={country}/>
+        ))}
+     </div>
+     </div>
+    )
+}
+
+const root =document.getElementById('root');
+ReactDOM.render(<App/>, root)
