7618cecba3ed3eb192724d15906761140024f3a6
saprunovdev <saprunovdev@gmail.com>
2022-12-16 17:21:05 +0900

    minor changes to day 4

--- day4/components/src/index.js
+++ day4/components/src/index.js
@@ -2,19 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const root = document.getElementById('root')
+
 function color() { 
   return '#' + Math.round(Math.random() * 0xffffff).toString(16)
 }
 
-const fixedColor = color()
-const ColorApp = () => (
-  <div>
-    <h1 style={{backgroundColor: fixedColor}}>{fixedColor}</h1>
-  </div>
-)
+
+const ColorApp = () => {
+  const fixedColor = color()
+  return(
+    <div>
+      <h1 style={{backgroundColor: color()}}>{fixedColor}</h1>
+    </div>
+  )
+}
+
 
 const App = () => (
   <div className='app'>
+    <ColorApp />
+    <ColorApp />
+    <ColorApp />
+    <ColorApp />
+    <ColorApp />
     <ColorApp />
   </div>
 )
